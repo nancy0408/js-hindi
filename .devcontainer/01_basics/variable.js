1eda2503b24de0bd2720d801355959d9a3416c73
nancy0408 <ghinaiyanensi2@gmail.com>
2025-04-03 03:24:27 +0000

    container config done

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = "12345"
+let accountEmail = "nensi@gmail.com"
+var accountPassword = "1234343"
+accountCity = "surat"
+
+console.log("accountId")
+console.table([accountId,accountEmail,accountPassword,accountCity])
